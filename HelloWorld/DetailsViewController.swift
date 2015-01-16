42d0f4a336aba075a6babff12d0aaf399811fa8d
Rommel Rico <rommeltj@gmail.com>
2015-01-16 09:50:17 -0800

    Broken

---
+++ HelloWorld/DetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailsViewController.swift
+//  HelloWorld
+//
+//  Created by Rommel Rico on 1/16/15.
+//  Copyright (c) 2015 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    var album: Album?
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
